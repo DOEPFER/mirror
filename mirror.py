c70d97534f9e103506004f1e28be86197dc0af00
Rafael Döepfer <doepfer.rafael@gmail.com>
2026-06-05 14:06:09 -0300

    parameters update

--- mirror.py
+++ mirror.py
@@ -11,41 +11,49 @@ from utils.ignore import ignore_dir
 from utils.validate import validate_source_mirror
 
 
-def sync_mirror(source: Path, mirror: Path, ignore: List[str]) -> None:
+def sync_mirror(source_path: Path, mirror_path: Path, ignore: List[str]) -> None:
 
-    if not validate_source_mirror(source_path=source, mirror_path=mirror):
+    if not validate_source_mirror(source_path=source_path, mirror_path=mirror_path):
         return
 
-    content_source = contents(source)
-    content_mirror = contents(mirror)
+    content_source = contents(source_path)
+    content_mirror = contents(mirror_path)
 
     if ignore:
         content_source = ignore_dir(content_source, ignore)
 
     # UPDATE
     to_update = dict()
-    to_update["is_dir"] = set(content_source["is_dir"]) & set(content_mirror["is_dir"])
-    to_update["is_dir"] = sorted(list(to_update["is_dir"]))  # type: ignore
-    to_update["is_file"] = set(content_source["is_file"]) & set(
-        content_mirror["is_file"]
+    to_update["is_dir"] = list(
+        set(content_source["is_dir"]) & set(content_mirror["is_dir"])
     )
-    update(source_path=source, mirror_path=mirror, contents=to_update)
+    to_update["is_dir"] = sorted(to_update["is_dir"])  # type: ignore
+    to_update["is_file"] = list(
+        set(content_source["is_file"]) & set(content_mirror["is_file"])
+    )
+    update(source_path=source_path, mirror_path=mirror_path, contents=to_update)
 
     # SEND
     to_send = dict()
-    to_send["is_dir"] = set(content_source["is_dir"]) - set(content_mirror["is_dir"])
-    to_send["is_dir"] = sorted(list(to_send["is_dir"]))  # type: ignore
-    to_send["is_file"] = set(content_source["is_file"]) - set(content_mirror["is_file"])
-    send(source_path=source, mirror_path=mirror, contents=to_send)
+    to_send["is_dir"] = list(
+        set(content_source["is_dir"]) - set(content_mirror["is_dir"])
+    )
+    to_send["is_dir"] = sorted(to_send["is_dir"])  # type: ignore
+    to_send["is_file"] = list(
+        set(content_source["is_file"]) - set(content_mirror["is_file"])
+    )
+    send(source_path=source_path, mirror_path=mirror_path, contents=to_send)
 
     # DELETE
     to_delete = dict()
-    to_delete["is_dir"] = set(content_mirror["is_dir"]) - set(content_source["is_dir"])
-    to_delete["is_dir"] = sorted(list(to_delete["is_dir"]), reverse=True)  # type: ignore
-    to_delete["is_file"] = set(content_mirror["is_file"]) - set(
-        content_source["is_file"]
+    to_delete["is_dir"] = list(
+        set(content_mirror["is_dir"]) - set(content_source["is_dir"])
     )
-    delete(mirror_path=mirror, contents=to_delete)
+    to_delete["is_dir"] = sorted(to_delete["is_dir"], reverse=True)  # type: ignore
+    to_delete["is_file"] = list(
+        set(content_mirror["is_file"]) - set(content_source["is_file"])
+    )
+    delete(mirror_path=mirror_path, contents=to_delete)
 
 
 logger = setup_logging(logprefix=args.logprefix, verbose=args.verbose)
@@ -54,4 +62,4 @@ if args.source and args.mirror:
     source_path = Path(args.source)
     mirror_path = Path(args.mirror)
 
-    sync_mirror(source=source_path, mirror=mirror_path, ignore=args.ignore)
+    sync_mirror(source_path=source_path, mirror_path=mirror_path, ignore=args.ignore)
